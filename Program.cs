c4bff3532eca2b15a9ea89d2521f7a6d7d5b101c
Oğuz Kaan Mavice <54191903+oguzkaan-mavice@users.noreply.github.com>
2020-10-11 01:26:01 +0300

    Immute Variables
    Immute Variables

--- Program.cs
+++ Program.cs
@@ -4,34 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NetFramework.S1.D2.ReferrenceTypeBehaviour
+namespace NetFramework.S1.D3.ImmutableVariables
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Car cardefinition1 = new Car();
-            cardefinition1.brand = "Opel";
-            cardefinition1.model = "Corsa";
-            cardefinition1.colour = "Blue";
+            int sayi1 = 10;
+            sayi1 = 111;
+            const int ortakbolumdeger = 2;//sabit değer olarak atadım.--const
+            int sonuc = sayi1 / ortakbolumdeger;
 
-            Car cardefinition2 = cardefinition1;
-
-            cardefinition2.colour = "Red";
-            cardefinition2.model = "Astra";
-            // cardefinition 2 ile cardefinition 1 ortak referans gösterdiğinden
-            // definition2 deki değişiklik ile definition 1 i de değiştirdim.
-            // çünkü bu iki tanımlama heap bölümünde ortak bir referansa gidiyor.
-
-
+            
         }
-    }
-
-    class Car
-    {
-        public string brand;
-        public string model;
-        public string colour;
-
     }
 }
