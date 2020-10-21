0098cc368c932f4dada0fa6d69adf05a62821c99
Oğuz Kaan Mavice <54191903+oguzkaan-mavice@users.noreply.github.com>
2020-10-21 17:39:34 +0300

    Add files via upload

---
+++ NetFramework.S6.D1.MethodApplication/Math.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S6.D1.MethodApplication
+{
+    public class Math
+    {
+        //sum
+        public decimal SumFunc(decimal Num1, decimal Num2)
+        {
+            decimal result = Num1 + Num2;
+            return result;
+        }
+
+        // diff
+
+        public decimal DiffFunc(decimal Num1, decimal Num2)
+        {
+            decimal result = Num1 - Num2;
+            return result;
+        }
+
+        // Divide
+
+        public decimal DivideFunc(decimal Num1,decimal Num2)
+        {
+            decimal result = Num1 / Num2;
+            return result;
+        }
+
+        //Multiply
+
+        public decimal MultiplyFunc(decimal Num1, decimal Num2)
+        {
+            decimal result = Num1 * Num2;
+            return result;
+        }
+
+        public void MenuCreate()
+        {
+            Console.Clear();
+            Console.WriteLine("***Menu***");
+            Console.WriteLine("1- Sum");
+            Console.WriteLine("2- Diff");
+            Console.WriteLine("3- Divide");
+            Console.WriteLine("4- Multiply");
+            Console.Write("Please choose a value");
+        }
+
+        public void resultPrint(decimal UserNum1, decimal UserNum2,decimal Result, string operators)
+        {
+            Console.WriteLine("{0} {1} {2} = {3}", UserNum1, operators, UserNum2, Result);
+        }
+    }
+}
---
+++ NetFramework.S6.D1.MethodApplication/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S6.D1.MethodApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math M = new Math();
+            AgainUseFunctions:
+            M.MenuCreate();
+            int UserPrefer = int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("Please enter the your first number");
+            decimal UserNum1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the your second number");
+            decimal UserNum2 = decimal.Parse(Console.ReadLine());
+
+            decimal result = 0;
+
+            switch (UserPrefer)
+            {
+                case 1:
+                    result=M.SumFunc(UserNum1, UserNum2);
+                    M.resultPrint(UserNum1, UserNum2, result, "+");
+                    break;
+                case 2:
+                    result = M.DiffFunc(UserNum1, UserNum2);
+                    M.resultPrint(UserNum1, UserNum2, result, "-");
+                    break;
+                case 3:
+                    result = M.DivideFunc(UserNum1, UserNum2);
+                    M.resultPrint(UserNum1, UserNum2, result, "/");
+                    break;
+                case 4:
+                    result = M.MultiplyFunc(UserNum1, UserNum2);
+                    M.resultPrint(UserNum1, UserNum2, result, "*");
+                    break;
+                default:
+                    Console.WriteLine("Your preference is not in menu");
+                    Console.WriteLine("Please, try again");
+                    System.Threading.Thread.Sleep(2000);
+                    goto AgainUseFunctions;
+            }
+
+            Console.WriteLine("Do you want to make another operation? E/H");
+            string EH = Console.ReadLine();
+            if (EH=="E")
+            {
+                goto AgainUseFunctions;
+            }
+        }
+    }
+}
