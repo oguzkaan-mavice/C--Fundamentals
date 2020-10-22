2a6f3fa3ad2d533c3daa1985867ed1e16222dfd0
Oğuz Kaan Mavice <54191903+oguzkaan-mavice@users.noreply.github.com>
2020-10-22 20:41:40 +0300

    Add files via upload

---
+++ NetFramework.S7.D1.OOP/Customer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S7.D1.OOP
+{
+    public class Customer
+    {
+        /*
+         * ID
+         * Name
+         * LastName
+         * Gender
+         */
+
+        //constructive method
+
+        public Customer()
+        {
+            ID = "1234345323";
+        }
+
+        public Customer(string _ID,string _Name)
+        {
+            ID = _ID;
+            Name = _Name;
+        }
+
+        public Customer(string _ID,string _Name, string _LastName)
+        {
+            ID = _ID;
+            Name = _Name;
+            LastName = _LastName;
+
+        }
+
+        public string ID;
+        public string Name;
+        public string LastName;
+        public int Gender;
+
+        public bool CustomerControl()
+        {
+            bool control = CustomerControlDB(ID);
+            return control;
+        } 
+
+        private bool CustomerControlDB(string ID)
+        {
+            return true;
+        }
+    }
+}
---
+++ NetFramework.S7.D1.OOP/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S7.D1.OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Customer C1 = new Customer();
+            Customer C1 = new Customer("21232454232", "Furkan");
+            C1.ID = "2233223387";
+            C1.Name = "Kaan";
+            C1.LastName = "Mavice";
+            C1.Gender = 15940001;
+
+            Customer C2 = C1;
+
+            C2.Name = "Mahmut";
+            C1.ID = "342349982";
+
+            bool customerControl = C1.CustomerControl();
+
+            C1=null;
+            C2=null;
+
+        }
+    }
+}
