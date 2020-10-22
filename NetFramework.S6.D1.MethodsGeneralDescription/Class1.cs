18fdda9f59f3e02e1946e652b4d4d970b97a3eb5
Oğuz Kaan Mavice <54191903+oguzkaan-mavice@users.noreply.github.com>
2020-10-22 18:28:15 +0300

    Add files via upload

---
+++ NetFramework.S6.D1.MethodsGeneralDescription/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S6.D1.MethodsGeneralDescription
+{
+    public class Student
+    {
+        public void StudentHi()
+        {
+            Console.WriteLine("Hi student");
+        }
+
+        /*
+         * 
+         * [Erişim belirleyicisi] <geri donus degeri> method adi(parametre)
+         * {
+         * }
+         * 
+         * Erişim belirleyicisi:
+         * 
+         * Public
+         * Private
+         * Protected
+         * Internal
+         * Internal Protected
+         *
+         * 
+         */
+
+        private void StudentMethod1(string StudentName, string StudentLastName)
+        {
+            Console.WriteLine("Student Info :  {0} {1}", StudentName, StudentLastName);
+        }
+
+        public void StudentMethod2()
+        {
+            StudentMethod1("Yekta", "Mavice");
+        }
+    }
+}
---
+++ NetFramework.S6.D1.MethodsGeneralDescription/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; //libraries
+
+namespace NetFramework.S6.D1.MethodsGeneralDescription
+{
+    class Program // packages
+    {
+        static void Main(string[] args) //methods
+        {
+            Hi();
+            Student O1 = new Student();
+            O1.StudentHi();
+            O1.StudentMethod2();
+        }
+
+        static void Hi()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
