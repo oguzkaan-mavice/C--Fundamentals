4c302cd2463609295e7a32004a68fdb4622a9be3
Oğuz Kaan Mavice <54191903+oguzkaan-mavice@users.noreply.github.com>
2020-10-27 12:55:32 +0300

    Add files via upload

---
+++ NetFramework.S8.D1.Static/StudentS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetFramework.S8.D1.Static
+{
+    public class StudentS
+    {
+        
+            public string Name { get; set; }
+            public string Lastname { get; set; }
+            public string EmailAdress { get; set; }
+
+            public void Test1()
+        {
+            Console.Write("Nesne ornegi alındıktan sonra calısan method");
+        }
+            public void Test2()
+        {
+            Console.WriteLine("Nesne ornegi alınmadan kullanılan method");
+        }
+    }
+}
